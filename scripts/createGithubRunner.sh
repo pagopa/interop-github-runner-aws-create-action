25d05104e8df4af77bf5b9934caa387da85794ca
Michele De Simone <michele.desimone@pagopa.it>
2024-07-01 16:33:05 +0200

    fix: print overrides

--- scripts/createGithubRunner.sh
+++ scripts/createGithubRunner.sh
@@ -46,10 +46,7 @@ echo "{\"containerOverrides\":[{\"name\":\"${ECS_CONTAINER_NAME}\",
 GROUP="${TARGET_ENV}-${RUNNER_NAME}"
 
 echo "Run task with GROUP:${GROUP} CLUSTER: ${ECS_CLUSTER_NAME} TASK DEF: ${ECS_TASK_DEFINITION}"
-echo "OVERRIDES: $(cat overrides.json)"
-
-echo "Run task with GROUP:${GROUP} CLUSTER: ${ECS_CLUSTER_NAME} TASK DEF: ${ECS_TASK_DEFINITION}"
-echo "OVERRIDES: $(cat overrides.json)"
+echo "OVERRIDES: $(cat overrides.json | sed -e "s/${REGISTRATION_TOKEN}/***/g")
 
 ECS_TASK_ID=$(aws ecs run-task \
   --launch-type "FARGATE" \
